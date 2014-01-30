ba4d6d589a6365d8386550c6170ad74c8c9edc89
prospegto <nacho.prosper@gmail.com>
2014-01-30 19:41:48 +0100

    Arreglado que se vea el sumario

--- src/es/nervion/maps/fragment/PreferenciasFragment.java
+++ src/es/nervion/maps/fragment/PreferenciasFragment.java
@@ -1,15 +1,60 @@
 package es.nervion.maps.fragment;
 
 import es.nervion.maps.activity.R;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 
-public class PreferenciasFragment extends PreferenceFragment {
-	
-	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class PreferenciasFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
 
-        addPreferencesFromResource(R.xml.fragment_preferences);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.fragment_preferences);
+
+		for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+			iniciarSumario(getPreferenceScreen().getPreference(i));
+		}
+	}
+
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+        String key) {
+      updatePreferences(findPreference(key));
     }
+
+
+	private void iniciarSumario(Preference p) {
+		if (p instanceof PreferenceCategory) {
+			PreferenceCategory cat = (PreferenceCategory) p;
+			for (int i = 0; i < cat.getPreferenceCount(); i++) {
+				iniciarSumario(cat.getPreference(i));
+			}
+		} else {
+			updatePreferences(p);
+		}
+	}
+
+	private void updatePreferences(Preference p) {
+		if (p instanceof EditTextPreference) {
+			EditTextPreference editTextPref = (EditTextPreference) p;
+			p.setSummary(editTextPref.getText());
+		}
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	}
+
 
 }
